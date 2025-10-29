06979e36193a802c9320560a803d6e0660fac43c
Julia Meller Mendes Silva <julia.3123@alunosatc.edu.br>
2025-10-29 19:55:25 -0300

    prova 2

--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,34 @@
-console.log("Hello, World! 002");
+//Adapter
+class LegacyPaymentSystem {
+    makePayment(amount) {
+      console.log(`Pagando R$${amount} com sistema legado.`);
+    }
+  }
+  
+  class ModernPaymentAPI {
+    process(amountInCents) {
+      console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+    }
+  }
+  
+  class ModernPaymentAdapter extends LegacyPaymentSystem {
+    constructor(modernAPI) {
+      super();
+      this.modernAPI = modernAPI;
+    }
+  
+    makePayment(amount) {
+      const amountInCents = amount * 100;
+      this.modernAPI.process(amountInCents);
+    }
+  }
+  
+  function pay(system, amount) {
+    system.makePayment(amount);
+  }
+  
+  // Cliente
+  const modernAPI = new ModernPaymentAPI();
+  const adaptedSystem = new ModernPaymentAdapter(modernAPI);
+  pay(adaptedSystem, 100);
+  
